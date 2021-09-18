ab5d19b38b27875b9e41989daa209c3738236e1d
Peppuss <64472824+Peppuss@users.noreply.github.com>
2021-09-18 12:09:16 +0200

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,1 +1,4 @@
-const { keep_alive } = require("./keep_alive");
+const express = require('express');
+const app = express();
+const port = 3000;
+app.listen(port, () => console.log(`Bot running on http://localhost:${port}`));
